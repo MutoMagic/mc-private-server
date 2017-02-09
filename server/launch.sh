36c4c76d06c313db9a5322126a8b237a07c33e81
muto <316663878@qq.com>
2017-02-09 13:00:49 +0800

    to 2g

--- server/launch.sh
+++ server/launch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd "$(dirname "$(readlink -fn "$0")")"
-java -Xms512m -Xmx1G -jar forge-1.8.9-11.15.1.1902-1.8.9-universal.jar
+java -Xms1G -Xmx2G -jar forge-1.8.9-11.15.1.1902-1.8.9-universal.jar
